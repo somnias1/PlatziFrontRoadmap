8c65959684e8a249ca064a9a9afbe5af85a4e9ca
Lizardo <lizardo@utp.edu.co>
2022-08-16 18:56:04 -0500

    Js practice from Fazt

---
+++ js_practice/fazt_react/index.js
@@ -0,0 +1,7 @@
+const title=document.createElement("h1")
+title.innerText = "Hola Mundo"
+
+const button = document.createElement("button")
+button.innerText = "click"
+document.body.append(title)
+document.body.append(button)
